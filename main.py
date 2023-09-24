4d19091113adca035a286c9e22c3c82074ff2212
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2023-09-24 18:09:54 +0530

    Support for adding other metadata to audio files

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import requests
 from dotenv import load_dotenv
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC, error
+from mutagen.easyid3 import EasyID3
 
 # Load environment variables from .env file
 load_dotenv()
@@ -72,17 +73,58 @@ def get_track_id(access_token, song_name, artist_name):
     return response.json()["tracks"]["items"][0]["id"]
 
 
-def get_cover_image_url(access_token, track_id):
-    """Get the cover image URL of a song from Spotify API"""
-    url = f"https://api.spotify.com/v1/tracks/{track_id}"
-    headers = {"Authorization": "Bearer " + access_token}
-    logging.info("Getting the cover image URL...")
-    response = requests.get(url, headers=headers, timeout=3)
-    return response.json()["album"]["images"][0]["url"]
+def get_metadata(access_token, track_id):
+    """Get the metadata of a song from Spotify API"""
+    try:
+        url = f"https://api.spotify.com/v1/tracks/{track_id}"
+        headers = {"Authorization": "Bearer " + access_token}
+        logging.info("Getting the cover image URL...")
+        response = requests.get(url, headers=headers, timeout=3)
+        track_image = response.json()["album"]["images"][0]["url"]
+        track_title = response.json()["name"]
+        track_artist = response.json()["artists"][0]["name"]
+        track_album = response.json()["album"].get("name", "")
+        track_number = response.json().get("track_number", "")
+        track_genre = response.json()["artists"][0].get("genres", "")
+        track_year = response.json()["album"].get("release_date", "")[:4]
+        return {
+            "image_url": track_image,
+            "title": track_title,
+            "artist": track_artist,
+            "album": track_album,
+            "track_number": track_number,
+            "genre": track_genre,
+            "year": track_year,
+        }
+    except Exception as e:
+        logging.error("Error occurred while getting metadata: %s", e)
+        print(f"--- ERROR: Failed to get metadata for track {track_id}. Skipping...")
+        return None
 
 
-def add_cover_image(song_file, image_url):
-    """Add cover image to a song file"""
+def replace_cover_image(audio, song_file, metadata):
+    """Replace the cover image of a song file"""
+    logging.info("Replacing the cover image...")
+    print("--- Replacing the cover image...")
+    audio.delete()
+    logging.info("Adding the cover image to %s...", song_file)
+    audio.tags.add(
+        APIC(
+            encoding=3,  # 3 is for utf-8
+            mime="image/jpeg",  # image/jpeg or image/png
+            type=3,  # 3 is for the cover image
+            desc="Cover",
+            data=requests.get(metadata.get("image_url"), timeout=3).content,
+        )
+    )
+    audio.save()
+
+
+def add_metadata(song_file, metadata, replace_cover_choice):
+    """Add metadata to a song file"""
+    if metadata is None:
+        return
+
     try:
         audio = MP3(song_file, ID3=ID3)
     except error:
@@ -97,32 +139,43 @@ def add_cover_image(song_file, image_url):
     except error:
         # If the ID3 tag already exists, pass
         pass
+    audio.save()
 
     # Check if the APIC tag exists
+    audio = MP3(song_file, ID3=ID3)
+
     if ("APIC:Cover" in audio) or ("APIC:" in audio):
         logging.warning("Cover image already exists in %s.", song_file)
-        choice = input(
-            "--- Existing cover image detected. Do you want to replace it? (y/n): "
-        )
-        if choice.lower() == "n":
-            logging.info("Skipping due to user intervention %s...", song_file)
-            print("--- File skipped.")
-            return
-        logging.info("Replacing the existing cover image...")
-        print("--- Replacing the existing cover image...")
-        audio.delete()
+        if replace_cover_choice == "1":
+            replace_cover_image(audio, song_file, metadata)
+        elif replace_cover_choice == "2":
+            choice = input(
+                "--- Existing cover image detected. Do you want to replace it? (y/n): "
+            )
+            if choice.lower() == "n":
+                logging.info(
+                    "Skip adding image due to user intervention %s...", song_file
+                )
+                print("--- Skipped adding cover image.")
+            else:
+                replace_cover_image(audio, song_file, metadata)
+        elif replace_cover_choice == "3":
+            pass
+    else:
+        replace_cover_image(audio, song_file, metadata)
 
-    logging.info("Adding the cover image to %s...", song_file)
-    audio.tags.add(
-        APIC(
-            encoding=3,  # 3 is for utf-8
-            mime="image/jpeg",  # image/jpeg or image/png
-            type=3,  # 3 is for the cover image
-            desc="Cover",
-            data=requests.get(image_url, timeout=3).content,
-        )
-    )
+    # Add metadata to the ID3 tag
+    logging.info("--- Adding metadata to %s...", song_file)
+    print("--- Adding metadata...")
+    audio = EasyID3(song_file)
+    audio["title"] = metadata.get("title")
+    audio["artist"] = metadata.get("artist")
+    audio["album"] = metadata.get("album")
+    audio["genre"] = metadata.get("genre")
+    audio["date"] = metadata.get("year")
+    audio["tracknumber"] = str(metadata.get("track_number"))
     audio.save()
+    logging.info("--- Metadata added successfully to %s.", song_file)
 
 
 def main():
@@ -135,18 +188,32 @@ def main():
       / _ \| | | |/ _` | |/ _ \    / _ \ | '__| __|
      / ___ \ |_| | (_| | | (_) |  / ___ \| |  | |_ 
     /_/   \_\__,_|\__,_|_|\___/  /_/   \_\_|   \__|
-    
+
     ----------------------------------------------------
-    This script adds cover images to your song files.
+    This script adds cover images and other metadata to your song files.
     Supported file formats: .mp3, .flac
 
-    Author: Dilshan-H 
+    Author: Dilshan-H
     Date: 2023-07-21
     License: MIT License
     ----------------------------------------------------
     """
     )
     print("Press Ctrl+C to exit the script.\n")
+    # prompt if user wants to be asked for whether to replace existing cover images
+    print(
+        "Should the existing covers to be replaced without asking?\n"
+        "----------------------------------------------------------\n"
+        "1: Yes, replace them all\n"
+        "2: No, ask me for each file\n"
+        "3: No, skip them all (keep the current covers)\n"
+    )
+    replace_cover_choice = input("Enter your choice (1/2/3): ").strip()
+    if replace_cover_choice not in ["1", "2", "3"]:
+        print("Invalid choice. Exiting...")
+        sys.exit(1)
+
+    logging.info("User chose option %s.", replace_cover_choice)
     access_token = get_access_token()
     folder = os.path.join(os.getcwd(), "songs")
     logging.info("Looking for song files in %s...", folder)
@@ -163,18 +230,24 @@ def main():
                     0
                 ]  # Use the current file name as the song name
                 logging.info("Processing song: %s...", song_name)
-                print(f"Processing song: {song_name[:50]}")
+                print(f"■ Processing song: {song_name[:50]}")
                 artist_name = ""
                 track_id = get_track_id(access_token, song_name, artist_name)
-                image_url = get_cover_image_url(access_token, track_id)
-                add_cover_image(os.path.join(folder, song_file), image_url)
+                metadata = get_metadata(access_token, track_id)
+                add_metadata(
+                    os.path.join(folder, song_file), metadata, replace_cover_choice
+                )
     except KeyboardInterrupt:
         logging.info("User interrupted the script.")
         print("\n---> Stopping the script...")
         sys.exit(0)
     except FileNotFoundError:
-        logging.error("The 'songs' directory does not exist in the current working directory.")
-        print("--- ERROR: The 'songs' directory does not exist in the current working directory.")
+        logging.error(
+            "The 'songs' directory does not exist in the current working directory."
+        )
+        print(
+            "--- ERROR: The 'songs' directory does not exist in the current working directory."
+        )
         logging.info("Stopping the script...")
         sys.exit(1)
     if file_count == 0:
